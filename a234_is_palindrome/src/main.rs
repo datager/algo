d78536929e39d3c247c05cd18662a70ac3f7c695
y <daska110@126.com>
2022-04-20 14:47:19 +0800

    feat: is_palindrome_bystask pass

--- a234_is_palindrome/src/main.rs
+++ a234_is_palindrome/src/main.rs
@@ -60,6 +60,75 @@ impl List {
 
         self
     }
+
+    // 链表反转
+    fn reverse(&mut self) {
+        let mut rnode: Link = self.head.take();
+        let mut lnode: Link = None;
+        while let Some(mut node) = rnode {
+            rnode = node.next; // rnode不断后移
+            node.next = lnode;
+            lnode = Some(node);
+        }
+        self.head = lnode;
+    }
+
+    // 找到链表中间节点
+    fn middle_node(&self) -> Option<&Box<Node>> {
+        if self.head.is_none() {
+            return None;
+        }
+
+        let mut fast = self.head.as_ref();
+        let mut slow = self.head.as_ref();
+
+        while fast.is_some() && fast.unwrap().next.is_some() {
+            slow = slow.unwrap().next.as_ref();
+            fast = fast.unwrap().next.as_ref().unwrap().next.as_ref();
+        }
+        slow
+    }
+
+    // 判断链表是否回文
+    fn is_palidrome_bystack(&self) -> bool {
+        if self.head.is_none() {
+            return true;
+        }
+        let mut cur = self.head.as_ref();
+
+        // 都放入栈(用数组实现)
+        let mut v = vec![];
+        while let Some(node) = cur {
+            v.push(node.elem);
+            cur = node.next.as_ref();
+        }
+
+        // 出栈
+        cur = self.head.as_ref();
+        for elem in v.into_iter().rev() {
+            let node = cur.unwrap();
+            if node.elem != elem {
+                return false;
+            }
+            cur = node.next.as_ref();
+        }
+
+        true
+    }
 }
 
-fn main() {}
+mod tests {
+    use super::List;
+    #[test]
+    fn test_is_palidrome_bystack() {
+        let mut list = List::new();
+        list.push(1).push(2).push(3).push(2).push(1);
+        let ret = list.is_palidrome_bystack();
+        println!("{}, {}", list, ret);
+        assert!(ret == true, "12321是回文字符串啊");
+    }
+}
+
+fn main() {
+    println!("rust写链表好难")
+}
